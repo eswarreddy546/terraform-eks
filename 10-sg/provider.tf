cb1c419d351586e9a7bacfdbd08143cb63ec1e3a
Asri <asri>
2026-03-21 19:19:15 +0530

    terraform eks

--- 10-sg/provider.tf
+++ 10-sg/provider.tf
@@ -7,10 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "remote-state-86s-dev"
+    bucket = "eshurr"
     key    = "roboshop-eks-sg"
     region = "us-east-1"
-    use_lockfile = true
     encrypt = true
   }
 }
