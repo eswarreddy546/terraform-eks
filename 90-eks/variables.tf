ac14469f38f6a5cbe1cb3bba528368813e03f3e9
Asri <asri>
2026-03-24 15:33:52 +0530

    terraform eks

--- 90-eks/variables.tf
+++ 90-eks/variables.tf
@@ -1,35 +1,49 @@
 variable "project_name" {
-    default = "roboshop"
+  description = "Project name"
+  type        = string
+  default     = "roboshop"
 }
 
 variable "environment" {
-    default = "dev"
+  description = "Environment name"
+  type        = string
+  default     = "dev"
 }
 
 variable "domain_name" {
-    default = "eswar.xyz"
+  description = "Route53 domain name"
+  type        = string
+  default     = "eswar.xyz"
 }
 
 variable "zone_id" {
-    default = "Z00224221HVTOMR4RK7YI"
+  description = "Route53 hosted zone ID"
+  type        = string
+  default     = "Z00224221HVTOMR4RK7YI"
 }
 
-#  variable "eks_version" {
-
-#  }
-
+# Node group versions
 variable "eks_nodegroup_blue_version" {
-
+  description = "Kubernetes version for blue node group"
+  type        = string
+  default     = "1.32"
 }
 
 variable "eks_nodegroup_green_version" {
-
+  description = "Kubernetes version for green node group"
+  type        = string
+  default     = "1.32"
 }
 
-variable enable_blue {
-
+# Enable flags (IMPORTANT 🔥)
+variable "enable_blue" {
+  description = "Enable blue node group"
+  type        = bool
+  default     = true
 }
 
-variable enable_green {
-    
+variable "enable_green" {
+  description = "Enable green node group"
+  type        = bool
+  default     = false
 }
